875ef7b20b6fccf0f3f4f4af487383a386a79f67
Christopher Koch <info@ckah.de>
2022-12-07 14:44:56 +0100

    add css styling and while loop for grid generation

--- script.js
+++ script.js
@@ -1,4 +1,65 @@
+const container = document.querySelector('#grid-container');
+let gridSize = 0;
+const div = document.createElement('div');
+
+function createGrid () {
+    
+    while (gridSize < 16) {
+        gridSize++;
+        console.log(gridSize);
+        const div = document.createElement('div');
+        container.append(div);
+    }
+    
+}
+
+createGrid();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // erstelle eine fläche innerhalb eines begrenzten containers
 // die Fläche ist in einheiten unterteilt
 
 // die fläche kann neu generiert werden
+
