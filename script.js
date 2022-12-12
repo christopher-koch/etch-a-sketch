a1b211b2bcad2e0f8cb6305d26e351d798ce3228
Christopher Koch <info@ckah.de>
2022-12-12 17:38:54 +0100

    assign button to colors and validate input field

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+let color = "black";
+
 function populateBoard(size) {
     let board = document.querySelector(".grid-container");
     let squares = board.querySelectorAll("div");
@@ -8,30 +10,28 @@ function populateBoard(size) {
     let amount = size * size;
     for (let i = 0; i < amount; i++) {
         let square = document.createElement("div");
-        square.style.backgroundColor = "blue";
+        square.addEventListener("mouseover", colorSquare);
+        square.style.backgroundColor = "white";
         board.insertAdjacentElement('afterbegin', square);
     }
 }
- 
-populateBoard(100);
 
+function checkInput(input) {
+    if(input >= 2 && input <=100) {
+        populateBoard(input);
+    } else {
+        console.log("Error");
+    }
+}
 
+function colorSquare () {
+    if(color === "random") {
+        this.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 50%)`;
+    } else {
+        this.style.backgroundColor = color;
+    }
+}
 
-// document.getElementById("container").getElementsByTagName("div").className += "square";
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+function changeColor(choice) {
+    color = choice;
+}
